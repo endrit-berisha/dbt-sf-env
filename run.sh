27daaf0b50ca8f1c32307153a128dbc91090f78e
endrit-berisha <endrit.berisha@sdggroup.com>
2024-04-16 10:29:01 -0400

    modify if

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker run --env-file .env --rm dbt-sf-env debug
 
-# Check if any .sql files have been modified
 if git diff --name-only --exit-code -- '*.yml' '*.sh' '*.sql'; then
     echo "No changes detected in DBT files. Skipping..."
 else
     echo "Changes detected in DBT files. Building Docker image and running DBT..."
     docker build -t dbt-sf-env .
+    docker run --env-file .env --rm dbt-sf-env debug
     docker run --env-file .env --rm dbt-sf-env compile
     docker run --env-file .env --rm dbt-sf-env run
 fi
