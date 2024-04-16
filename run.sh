94e2e7e6077b6e97243e0f0352ebfee1d3646b2d
endrit-berisha <endrit.berisha@sdggroup.com>
2024-04-16 09:41:32 -0400

    building or running based on modification

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$(git status --porcelain)" ]; then
+    echo "files modified!  --building and running..."
+    docker build -t dbt-sf-env .
+    docker run --env-file .env --rm dbt-sf-env run
+else
+    echo "no files modified! --running..."
+    docker run --env-file .env --rm dbt-sf-env run
+fi
