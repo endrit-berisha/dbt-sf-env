6cb31b6e5a008b5a33a6c29f120760c96e50ee8a
endrit-berisha <endrit.berisha@sdggroup.com>
2024-04-16 10:10:04 -0400

    add compile for sql models

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 if [ "$(git status --porcelain)" ]; then
     echo "files modified!  --building and running..."
     docker build -t dbt-sf-env .
+    docker run --env-file .env --rm dbt-sf-env compile
     docker run --env-file .env --rm dbt-sf-env run
 else
     echo "no files modified! --running..."
+    docker run --env-file .env --rm dbt-sf-env compile
     docker run --env-file .env --rm dbt-sf-env run
 fi
