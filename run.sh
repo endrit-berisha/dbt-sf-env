beddce07acc05b09c1f28e52c6e7a50cf90df4ba
endrit-berisha <endrit.berisha@sdggroup.com>
2024-04-16 10:25:10 -0400

    detect changes and build if detected

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+docker run --env-file .env --rm dbt-sf-env debug
 
-if [ "$(git status --porcelain)" ]; then
-    echo "files modified!  --building and running..."
-    docker build -t dbt-sf-env .
-    docker run --env-file .env --rm dbt-sf-env compile
-    docker run --env-file .env --rm dbt-sf-env run
+# Check if any .sql files have been modified
+if git diff --name-only --exit-code -- '*.yml' '*.sh' '*.sql'; then
+    echo "No changes detected in DBT files. Skipping..."
 else
-    echo "no files modified! --running..."
+    echo "Changes detected in DBT files. Building Docker image and running DBT..."
+    docker build -t dbt-sf-env .
     docker run --env-file .env --rm dbt-sf-env compile
     docker run --env-file .env --rm dbt-sf-env run
 fi
